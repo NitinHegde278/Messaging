9615815a34f86fb0d0ec6fd0f225528f314f4dde
nitin <nhnitinhegde24@gmail.com>
2020-08-05 18:57:25 +0530

    home and work states

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production: false,
-    // backendUrl: "/",
-    backendUrl: "http://localhost:3000/",
-    messageCreditAuth : '335034AEqrcevx5f05d8c1P1',
-    apiBase: '/assets/data/',
-    playStoreUrl:'',
-  };
